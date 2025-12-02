c69b6cec6f24494f02aac2b801f6f85fac6ba7eb
kepha-wiz <kephacheps55@gmail.com>
2025-12-02 13:00:02 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const { createServer } = require("http");
+const next = require("next");
+const { initSocket } = require("./src/socket");
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    handle(req, res);
+  });
+
+  // attach socket.io
+  initSocket(server);
+
+  server.listen(port, () => {
+    console.log(`Server with Socket.IO running on port ${port}`);
+  });
+});
